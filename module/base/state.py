93be14c0370f89f665d85461645e21df17a83607
zqh <zqh@zqh.com>
2022-05-27 10:22:12 +0800

    fix close game bug

--- module/base/state.py
+++ module/base/state.py
@@ -28,19 +28,23 @@ class State:
     @classmethod
     def job_start(cls, job):
         if cls.running_job_num == 0:
+            logger.debug("job_start:set running_job:" + str(job.id) + str(job.name))
             cls.running_job['id'] = job.id
             cls.running_job['name'] = job.name
             cls.running_job_num += 1
         else:
+            logger.debug("job_start:add blocking_jobs:" + str(job.id) + str(job.name))
             cls.blocking_jobs.append(job)
 
     @classmethod
     def job_finish(cls):
         if len(cls.blocking_jobs) != 0:
             job = cls.blocking_jobs.pop(0)
+            logger.debug("job_start:remove blocking_jobs to running_job:" + str(job.id) + str(job.name))
             cls.running_job['id'] = job.id
             cls.running_job['name'] = job.name
         else:
+            logger.debug("job_start:remove  running_job:" + str(cls.running_job['id']) + str(cls.running_job['name']))
             cls.running_job_num -= 1
             cls.running_job['id'] = ""
             cls.running_job['name'] = ""
