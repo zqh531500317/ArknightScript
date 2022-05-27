fca67c25fcae9fd8415af9adc4e3f2ed1576a82c
zqh <zqh@zqh.com>
2022-05-27 11:09:21 +0800

    fix close game bug

--- Arknight-Script.py
+++ Arknight-Script.py
@@ -1,6 +1,7 @@
-from module.app import create_app
 
-app = create_app()
 # 支持 py installer
 if __name__ == '__main__':
+    from module.app import create_app
+
+    app = create_app()
     app.run(host='0.0.0.0')
