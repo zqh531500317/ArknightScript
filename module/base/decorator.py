85e2b041a8a55cb82b35da4ea62e9d061bc35052
zqh <zqh@zqh.com>
2022-05-27 11:15:59 +0800

    fix close game bug

--- module/base/decorator.py
+++ module/base/decorator.py
@@ -16,13 +16,16 @@ def singleton(cls):
 
 
 def before(func):
-    def wrapper():
+    @wraps(func)
+    def wrapper(*args, **kwargs):
         from module.base.base import base
         from module.schedule.listener import listener
         task_name = base.state.running_job['name']
         listener.caltimemap[task_name]['start_time'] = datetime.datetime.now().replace(microsecond=0)
         logger.info("task %s is started", task_name)
-        return func()
+        res = func(*args, **kwargs)
+        return res
+
     return wrapper
 
 
